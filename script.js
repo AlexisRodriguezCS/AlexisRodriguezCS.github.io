fef7c59afb19616f17c3401be90f897c45e4ae29
Alexis Rodriguez <alexisrodriguezjs21@gmail.com>
2022-06-30 13:58:37 -0500

    Updated nav bar
    Nav bar has js so it hides/unhides when scrolling plus add some style options

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+// Reset form after hitting submit
 window.onbeforeunload = () => {
   for (const form of document.getElementsByTagName("form")) {
     form.reset();
   }
 };
+// Nav bar hide/unhide when scrolling
+const nav = document.querySelector(".nav");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    // Scrolling down
+    nav.classList.add("hide");
+  } else {
+    // Scrolling up
+    nav.classList.remove("hide");
+  }
+  lastScrollY = window.scrollY;
+});
